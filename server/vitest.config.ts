b7a433bfb8e742ecafbdfe78b0c3f98f0ef48082
Ishwar830 <easwersuthar830@gmail.com>
2026-02-14 12:52:37 +0530

    Add integration tests for Quiz Repository

--- server/vitest.config.ts
+++ server/vitest.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     globals: true,
-    globalSetup: ["./src/__test__/setup.ts"],
+    globalSetup: ["./src/__tests__/setup.ts"],
   },
 });
